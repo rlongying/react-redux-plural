fde055a608236a0f1e43ef909b2ce052b4dfdb33
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2021-04-04 20:31:59 -0700

    Basic pages layout: home, about, courses

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Courses</h1>;
+  }
+}
+
+export default CoursesPage;
