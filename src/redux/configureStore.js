133ec1423a3772db2bf168a832cfa30f3ad846ef
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2021-04-13 22:19:22 -0700

    add redux and course action/reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) {
+  // add support for redux dev tools
+  const composeEnhancer =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancer(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+}
