5c82cdfee29aed9ad4345e0d22028f37bd9de642
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2021-05-05 23:18:10 -0700

    add production config

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-  // add support for redux dev tools
-  const composeEnhancer =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancer(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
 }
